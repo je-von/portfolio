e9b3d97051f0a70816e1b0b42d38c219d50994ca
jevon <jevon.levin@gamil.com>
2022-05-17 17:07:36 +0700

    show readme

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -2,11 +2,12 @@ import type { NextComponentType } from 'next'
 
 import Link from 'next/link'
 import { useState } from 'react'
-import { AiFillEye, AiOutlineGithub } from 'react-icons/ai'
+import { AiFillEye, AiOutlineClose, AiOutlineGithub } from 'react-icons/ai'
 import { BsArrowDown, BsInfoCircle } from 'react-icons/bs'
 import { HiArrowDown } from 'react-icons/hi'
-import { MdArrowDownward, MdArrowDropDown } from 'react-icons/md'
+import { MdArrowDownward, MdArrowDropDown, MdClose } from 'react-icons/md'
 import ReactMarkdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
 import useSWR from 'swr'
 import { Data } from '../../@types/prop.types'
 import { fetcher } from '../../lib/fetcher'
@@ -15,7 +16,40 @@ import Tooltip from '../Misc/Tooltip'
 const Projects: NextComponentType = () => {
   const { data } = useSWR<Data>('/api/github', fetcher)
   const [count, setCount] = useState(3)
-  const [showModal, setShowModal] = useState(false)
+  const [repo, setRepo] = useState(null)
+
+  const modal = (
+    <>
+      <div className="fixed inset-0 z-50 mt-10 flex h-[90vh] items-center justify-center overflow-x-hidden text-black outline-none focus:outline-none">
+        <div className="relative my-6 mx-auto h-[90vh] w-auto max-w-[90vw] sm:max-w-[50vw]">
+          {/*content*/}
+          <div className="relative flex  w-full flex-col rounded-lg border-0 bg-gray-900 shadow-lg outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
+              <h3 className="flex text-3xl font-semibold">
+                {repo?.name} <p className="ml-2 font-thin text-gray-500">README</p>
+              </h3>
+              <button
+                className="float-right ml-auto border-0 p-1 text-2xl leading-none outline-none focus:outline-none"
+                onClick={() => setRepo(null)}
+              >
+                <MdClose className="text-white" />
+              </button>
+            </div>
+            {/*body*/}
+            <div className="relative w-full flex-auto overflow-hidden p-6">
+              <p className="my-4 w-full break-words text-lg leading-relaxed text-slate-500">
+                <ReactMarkdown className="text-white" rehypePlugins={[rehypeRaw]}>
+                  {repo?.readme}
+                </ReactMarkdown>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+    </>
+  )
 
   console.log(data)
   return (
@@ -36,11 +70,10 @@ const Projects: NextComponentType = () => {
       <div className="my-4 flex w-full flex-wrap items-center justify-center">
         {data?.repositories.slice(0, count).map((repo: any) => (
           <div
-            onClick={() => setShowModal(true)}
+            onClick={() => setRepo(repo)}
             key={repo.id}
-            className="mx-2 my-4  min-h-fit min-w-[16rem] shrink-0 grow-0 basis-1/4 rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
+            className="mx-2 my-4 min-h-fit  min-w-[16rem] shrink-0 grow-0 basis-1/4 cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
           >
-            {/* <ReactMarkdown>{repo.readme}</ReactMarkdown> */}
             <div className="flex h-full min-h-[12rem] w-full flex-col items-center justify-center rounded-lg bg-white px-2 text-center font-medium text-black dark:bg-black dark:text-white">
               <p className="text-xl font-semibold">{repo.name}</p>
               <p className="max-h-2/4 w-full text-ellipsis line-clamp-2">{repo.description}</p>
@@ -92,49 +125,7 @@ const Projects: NextComponentType = () => {
           {count < data?.repositories.length ? <MdArrowDropDown /> : <AiOutlineGithub />}
         </button>
       </div>
-      {showModal ? (
-        <>
-          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-            <div className="relative my-6 mx-auto w-auto max-w-3xl">
-              {/*content*/}
-              <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
-                  <button
-                    className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">×</span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative flex-auto p-6">
-                  <p className="my-4 text-lg leading-relaxed text-slate-500">asd</p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
-                  <button
-                    className="background-transparent mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="mr-1 mb-1 rounded bg-emerald-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-        </>
-      ) : null}
+      {repo ? modal : ''}
     </div>
   )
 }
