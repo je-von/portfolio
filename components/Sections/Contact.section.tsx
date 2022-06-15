dac21f129e70a7f449b84d6a782979f6e4034689
jevon <jevon.levin@gamil.com>
2022-06-15 20:21:21 +0700

    update icon

--- components/Sections/Contact.section.tsx
+++ components/Sections/Contact.section.tsx
@@ -4,13 +4,14 @@ import { BsSpotify, MdEmail, AiOutlineGithub, AiOutlineTwitter } from '../Misc/I
 
 import { Icon } from '../Misc/Icon.component'
 import { AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
+import { FaLinkedin } from 'react-icons/fa'
 
 const Contact: NextComponentType = () => {
   return (
     <div className="px-3 font-sen" id="contact">
       <div className="my-8 flex flex-row justify-center gap-x-4">
         <Icon icon={<AiOutlineGithub />} url="https://github.com/je-von" />
-        <Icon icon={<AiFillLinkedin />} url="https://www.linkedin.com/in/jevon-levin/" />
+        <Icon icon={<FaLinkedin />} url="https://www.linkedin.com/in/jevon-levin/" />
 
         <Icon icon={<MdEmail />} url="mailto:jevon.levin@gmail.com" />
 
